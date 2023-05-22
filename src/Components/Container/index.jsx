587e0d9801b9bc805ce9a7830ba34203348d177b
Angelo <angelo100492@gmail.com>
2023-05-21 21:59:04 -0300

    Finalizado, con temas claro y oscuro

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-
+import styled from "styled-components";
 import Title from "../Title";
 import Account from "../Account";
+import List from "../List";
+
+const StyledContainer = styled.div`
+background-color: ${({theme}) => theme.body};
+min-height: 90vh;
+padding: 0px 15vw;
+  `
+
+const StyledContent = styled.section`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+@media (max-width: 800px) {
+  flex-direction: column;
+}
+
+
+`
 
 const Container = () => {
   return (
-    <div className="container">
+    <StyledContainer>
       <Title>Smart Bank</Title>
-      <section className="content">
+      <StyledContent>
         <Account />
-      </section>
-    </div>
+        <List/>
+      </StyledContent>
+    </StyledContainer>
   );
 };
 
